28e47bdfe8fa5a73a054729b4168e39bc5f30f5d
Judit Vives <jvivesramon@gmail.com>
2023-04-16 23:04:40 +0200

    Add timer to recursive function stillPlaying

--- app/index.js
+++ app/index.js
@@ -2,4 +2,6 @@ import getCellTable from "./getCellTable/getCellTable.js";
 import getcurrentChangesTableCell from "./getCurrenChangesTableCell/getCurrenChangesTableCell.js";
 import stillPlaying from "./stillPlaying/stillPlaying.js";
 
-stillPlaying(getcurrentChangesTableCell(getCellTable(18, 30)));
+const wait = stillPlaying(getcurrentChangesTableCell(getCellTable(18, 30)));
+
+export default wait;
